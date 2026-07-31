52823553753a2c85ebe5e5afaa162e7983439874
MrSound <138537198+Aref-sd@users.noreply.github.com>
2026-07-31 19:17:57 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,9 @@
 export default {
   async fetch(request) {
-    return new Response("WORKER TEST SUCCESS", {
+    return new Response("WORKER IS RUNNING", {
       status: 200,
       headers: {
-        "content-type": "text/plain"
+        "content-type": "text/plain;charset=UTF-8"
       }
     });
   }
