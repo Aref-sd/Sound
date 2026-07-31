067954edd11f9d4ef6be45f6830dbd0491833bd9
MrSound <138537198+Aref-sd@users.noreply.github.com>
2026-07-31 18:09:34 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,1 +1,9 @@
-
+export default {
+  async fetch(request) {
+    return new Response("Sound-CBV Worker is running", {
+      headers: {
+        "content-type": "text/plain"
+      }
+    });
+  }
+};
